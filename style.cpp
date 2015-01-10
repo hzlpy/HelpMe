4d7880568ffeb36e59a725ce87c8fe35916733a6
Happy <iamchuzhiyan@gmail.com>
2015-01-10 14:51:48 +0800

    package style into a class

--- style.cpp
+++ style.cpp
@@ -3,3 +3,39 @@
 Style::Style()
 {
 }
+
+/**
+ * @brief Style::setId
+ * @param id
+ */
+void Style::setId(QString id)
+{
+    this->id = id;
+}
+
+/**
+ * @brief Style::getId
+ * @return id
+ */
+QString Style::getId()
+{
+    return this->id;
+}
+
+/**
+ * @brief Style::setName
+ * @param name
+ */
+void Style::setName(QString name)
+{
+    this->name = name;
+}
+
+/**
+ * @brief Style::getName
+ * @return name
+ */
+QString Style::getName()
+{
+    return this->name;
+}
--- style.h
+++ style.h
@@ -1,10 +1,17 @@
 #ifndef STYLE_H
 #define STYLE_H
-
+#include <QString>
 class Style
 {
 public:
     Style();
+    void setId(QString id);
+    QString getId();
+    void setName(QString name);
+    QString getName();
+private:
+    QString id;
+    QString name;
 };
 
 #endif // STYLE_H
